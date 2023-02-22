2aa23ec30f720a794abd4f3499ea52befa10f397
NovbiChannel <chaglovnikita@gmail.com>
2023-02-22 22:58:46 +0500

    token regenerate

--- app/src/main/java/com/example/test_task_followers/data/api/Api.kt
+++ app/src/main/java/com/example/test_task_followers/data/api/Api.kt
@@ -9,25 +9,25 @@ import retrofit2.http.Query
 
 interface Api {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getSearchUsers(
         @Query("q") query: String
     ): Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getUserDetail(
         @Path("username") username: String,
     ): Call<DetailUserResponce>
 
     @GET("users/{username}/repos")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getReposUser(
         @Path("username") username: String
     ): Call<ArrayList<ReposUserResponce>>
 
     @GET("users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getAllUser(
         @Query("since") since: Int = 0,
     ): Call<ArrayList<User>>
